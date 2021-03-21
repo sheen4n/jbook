78492196e0227881f388beea4c337b0d00d0c850
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2021-03-21 10:57:00 +0800

    Reset code editor before eval code

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import { unpkgPathPlugin } from './plugins/unpkg-path-plugins';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -23,6 +23,8 @@ const App = () => {
   const onClick = async () => {
     if (!ref.current) return;
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -34,10 +36,29 @@ const App = () => {
       },
     });
 
-    // console.log(result);
-
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+  <html>
+  <head></head>
+  <body>
+  <div id="root"></div>
+  <script>
+  window.addEventListener('message', (event) => {
+    try{
+      eval(event.data);
+    } catch (err){
+      const root = document.querySelector('#root');
+      root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+      console.err(err);
+    }
+  }, false);
+  </script>
+  </body>
+  </html>
+  `;
 
   return (
     <div>
@@ -45,7 +66,7 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe title='preview' srcDoc={html} sandbox='allow-scripts' ref={iframe} />
     </div>
   );
 };
