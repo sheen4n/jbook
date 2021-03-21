c0f3ab4f8689cb21118bb69ed0548dc114d4c3eb
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2021-03-21 11:16:11 +0800

    Switched Off Minimap and wordWrap

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,19 @@
 import MonacoEditor from '@monaco-editor/react';
 
 const CodeEditor = () => {
-  return <MonacoEditor height='500px' />;
+  return (
+    <MonacoEditor
+      height='500px'
+      language='javascript'
+      theme='dark'
+      options={{
+        wordWrap: 'on',
+        minimap: {
+          enabled: false,
+        },
+      }}
+    />
+  );
 };
 
 export default CodeEditor;
