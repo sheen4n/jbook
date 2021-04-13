e5848ca731e09e838a8778270348c95fb5ec9342
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2021-04-13 15:44:40 +0800

    Create format button

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -2,6 +2,7 @@ import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
 import prettier from 'prettier';
 import parser from 'prettier/parser-babel';
 import { useRef } from 'react';
+import './code-editor.css';
 
 interface CodeEditorProps {
   initialValue: string;
@@ -25,21 +26,25 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
     const unformatted = editorRef.current.getModel().getValue();
 
     // format that value
-    const formatted = prettier.format(unformatted, {
-      parser: 'babel',
-      plugins: [parser],
-      useTabs: false,
-      semi: true,
-      singleQuote: true,
-    });
+    const formatted = prettier
+      .format(unformatted, {
+        parser: 'babel',
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      })
+      .replace(/\n$/, '');
 
     // set the formatted value back in the editor
     editorRef.current.setValue(formatted);
   };
 
   return (
-    <>
-      <button onClick={onFormatClick}>Format</button>
+    <div className='editor-wrapper'>
+      <button className='button button-format is-primary is-small' onClick={onFormatClick}>
+        Format
+      </button>
       <MonacoEditor
         editorDidMount={onEditorDidMount}
         value={initialValue}
@@ -57,7 +62,7 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
           automaticLayout: true,
         }}
       />
-    </>
+    </div>
   );
 };
 
