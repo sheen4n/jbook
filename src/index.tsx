75ecb8fd341845d5c0716b770ef2bdff89a0189d
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2021-04-21 14:16:32 +0800

    Code refactor for bundler and preview

--- src/index.tsx
+++ src/index.tsx
@@ -1,75 +1,27 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import * as esbuild from 'esbuild-wasm';
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { fetchPlugin } from './plugins/fetch-plugin';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugins';
+
 import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
+import bundle from './bundler';
 
 const App = () => {
-  const ref = useRef<any>();
-  const iframe = useRef<any>();
+  const [code, setCode] = useState('');
   const [input, setInput] = useState('');
 
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
   const onClick = async () => {
-    if (!ref.current) return;
-
-    iframe.current.srcdoc = html;
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    });
-
-    // setCode(result.outputFiles[0].text);
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+    const output = await bundle(input);
+    setCode(output);
   };
-
-  const html = `
-  <html>
-  <head></head>
-  <body>
-  <div id="root"></div>
-  <script>
-  window.addEventListener('message', (event) => {
-    try{
-      eval(event.data);
-    } catch (err){
-      const root = document.querySelector('#root');
-      root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
-      console.err(err);
-    }
-  }, false);
-  </script>
-  </body>
-  </html>
-  `;
 
   return (
     <div>
       <CodeEditor onChange={(value) => setInput(value)} initialValue='const a = 1;' />
-      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe title='preview' srcDoc={html} sandbox='allow-scripts' ref={iframe} />
+      <Preview code={code} />
     </div>
   );
 };
