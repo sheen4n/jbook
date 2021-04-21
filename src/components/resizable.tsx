97d4e52b7ae8208abc3c0af5bc9f4169fe94c9cb
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2021-04-21 16:20:00 +0800

    Added responsiveness to cell resizing

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react';
-import { ResizableBox } from 'react-resizable';
+import { FC, useEffect, useState } from 'react';
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 import './resizable.css';
 
 interface ResizableProps {
@@ -7,11 +7,49 @@ interface ResizableProps {
 }
 
 const Resizable: FC<ResizableProps> = ({ direction, children }) => {
-  return (
-    <ResizableBox height={300} width={Infinity} resizeHandles={['s']}>
-      {children}
-    </ResizableBox>
-  );
+  let resizableProps: ResizableBoxProps;
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    let timer: any;
+    const listener = () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+
+      timer = setTimeout(() => {
+        setInnerWidth(window.innerWidth);
+        setInnerHeight(window.innerHeight);
+      }, 100);
+    };
+    window.addEventListener('resize', listener);
+
+    return () => {
+      window.removeEventListener('resize', listener);
+    };
+  }, []);
+
+  if (direction === 'horizontal') {
+    resizableProps = {
+      className: 'resize-horizontal',
+      width: innerWidth * 0.75,
+      height: Infinity,
+      minConstraints: [innerWidth * 0.2, Infinity],
+      maxConstraints: [innerWidth * 0.75, Infinity],
+      resizeHandles: ['e'],
+    };
+  } else {
+    resizableProps = {
+      width: Infinity,
+      height: 300,
+      minConstraints: [Infinity, 24],
+      maxConstraints: [Infinity, innerHeight * 0.9],
+      resizeHandles: ['s'],
+    };
+  }
+
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;
