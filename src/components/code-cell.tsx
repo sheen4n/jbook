f7e2fe74f95cf020e4a8f1d01c6f8ddc8fb27c6f
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2021-04-21 17:16:06 +0800

    Added automatic execution

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import CodeEditor from './code-editor';
 import Preview from './preview';
@@ -9,10 +9,16 @@ const CodeCell = () => {
   const [code, setCode] = useState('');
   const [input, setInput] = useState('');
 
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [input]);
 
   return (
     <Resizable direction='vertical'>
