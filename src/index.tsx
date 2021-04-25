cd1a20258819c23c293dd6744d5f88fc1897edbb
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2021-04-25 16:38:13 +0800

    Add Redux with Immer

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
+import { store } from './state';
 // import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
+import { Provider } from 'react-redux';
 
 const App = () => {
   return (
-    <div>
-      {/* <CodeCell /> */}
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        {/* <CodeCell /> */}
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,39 @@
+import { ActionType } from '../action-types';
+import {
+  Action,
+  Direction,
+  UpdateCellAction,
+  DeleteCellAction,
+  MoveCellAction,
+  InsertCellBeforeAction,
+} from '../actions';
+import { CellTypes } from '../cell';
+
+export const updateCell = (id: string, content: string): UpdateCellAction => ({
+  type: ActionType.UPDATE_CELL,
+  payload: {
+    id,
+    content,
+  },
+});
+
+export const deleteCell = (id: string): DeleteCellAction => ({
+  type: ActionType.DELETE_CELL,
+  payload: id,
+});
+
+export const moveCell = (id: string, direction: Direction): MoveCellAction => ({
+  type: ActionType.MOVE_CELL,
+  payload: {
+    id,
+    direction,
+  },
+});
+
+export const insertcellBefore = (id: string, cellType: CellTypes): InsertCellBeforeAction => ({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id,
+    type: cellType,
+  },
+});
