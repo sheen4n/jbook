16594194a4ca5564f884f0187d60009596863130
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2021-05-01 13:03:30 +0800

    Add Cumulative Code Feature

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -14,21 +14,32 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+  const cumulativeCode = useTypedSelector((state) => {
+    const { data, order } = state.cells;
+    const orderedCells = order.map((id) => data[id]);
+
+    const cumulativeCode = [];
+    for (let c of orderedCells) {
+      if (c.type === 'code') cumulativeCode.push(c.content);
+      if (c.id === cell.id) break;
+    }
+    return cumulativeCode;
+  });
 
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join('\n'));
     }
 
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join('\n'));
     }, 750);
 
     return () => {
       clearInterval(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [createBundle, cell.id, cell.content]);
+  }, [createBundle, cell.id, cumulativeCode]);
 
   return (
     <Resizable direction='vertical'>
