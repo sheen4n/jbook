06fe219ea2ec029d2000ef82c35da0da39cfa03e
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2021-05-14 17:54:02 +0800

    Rename packages and relink via lerna bootstrap

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { Command } from 'commander';
-import { serve } from 'local-api';
+import { serve } from '@sheen4n-jsnote/local-api';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
